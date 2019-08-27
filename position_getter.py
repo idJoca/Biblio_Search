2b5a0cb414c3ec2581142a3e5062d427ad1674c2
Josaid <brazjoaopedro@ymail.com>
2019-08-27 17:31:17 -0300

    Finished gattering the data
    The complete.csv is finally integrated with the  books position

--- position_getter.py
+++ position_getter.py
@@ -2,7 +2,7 @@ import csv
 import re
 
 complete_csv_path = 'complete_utf.csv'
-raw_csv_path = 'tombo.csv'
+raw_csv_path = 'exemplares.csv'
 position_csv_path = 'posicao.csv'
 new_complete = 'new_complete.csv'
 with open(complete_csv_path, encoding='utf-8') as complete:
@@ -15,7 +15,8 @@ with open(complete_csv_path, encoding='utf-8') as complete:
                 new_header = complete_reader.fieldnames.copy()
                 new_header = new_header + ['bandeja',\
                                            'coluna',\
-                                           'estante']
+                                           'estante',
+                                           'lado']
                 new_complete_csv = csv.DictWriter(new_complete, new_header)
                 new_complete_csv.writeheader()
                 used_cutters = []
@@ -26,15 +27,34 @@ with open(complete_csv_path, encoding='utf-8') as complete:
                             any(this_cutter == x for x in used_cutters)):
                             this_cdd = raw_row['CDD']
                             used_cutters.append(this_cutter)
+                            # raw.seek(0)
                             break
                     for pos_row in pos_reader:
-                        start_cdd = list(filter(None, re.split("[A-Z]", pos_row['de'])))
-                        end_cdd = list(filter(None, re.split("[A-Z]", pos_row['ate'])))
-                        if (any([this_cdd >= x for x in start_cdd]) and
-                            any([this_cdd >= x for x in end_cdd])):
+                        letters = re.findall('[A-Z]', this_cdd)
+                        if (pos_row['de'] == '' or pos_row['ate'] == ''):
+                            continue
+                        start_cdd = re.findall("[0-9.0-9]+", pos_row['de'])
+                        try:
+                            start_cdd_float = float(start_cdd[0])
+                        except Exception:
+                            continue
+                        end_cdd = re.findall("[0-9.0-9]+", pos_row['ate'])
+                        try:
+                            end_cdd_float = float(end_cdd[0])
+                        except Exception:
+                            continue
+                        try:
+                            this_cdd_float = float(this_cdd)
+                        except Exception:
+                            continue
+                        if (this_cdd_float >= start_cdd_float and
+                            this_cdd_float <= end_cdd_float and
+                            (any([x in pos_row['de'] or x in pos_row['ate'] for x in letters]) or len(letters) == 0)):
                             row['bandeja'] = pos_row['bandeija']
                             row['coluna'] = pos_row['coluna']
                             row['estante'] = pos_row['estante']
+                            row['lado'] = pos_row['lado']
                             new_complete_csv.writerow(row)
                             break
+                    pos_reader.line_num = 0
                     position.seek(0)
